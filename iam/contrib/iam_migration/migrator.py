835974070be5979cf2493f5ff7de4cfd2f353f12
nannan00 <17491932+nannan00@users.noreply.github.com>
2025-05-26 15:54:35 +0800

    feat: support IAMMigrator config tenant_id  and disable urllib3 warn

--- iam/contrib/iam_migration/migrator.py
+++ iam/contrib/iam_migration/migrator.py
@@ -36,6 +36,26 @@ class IAMMigrator(object):
         self.migration_json = migration_json
         self._bk_app_code = getattr(settings, "APP_CODE", "")
         self._bk_app_secret = settings.SECRET_KEY
+        self._bk_app_tenant_id = self.get_tenant_id()
+
+    @staticmethod
+    def get_tenant_id():
+        """
+        获取应用所属的租户 ID
+        Note: BKPAAS_APP_TENANT_ID 和 BK_APP_TENANT_ID 的含义不一样
+            BKPAAS_APP_TENANT_ID 是应用的租户模式标识，表示应用是全租户还是单租户
+            BK_APP_TENANT_ID 是应用所属的租户 ID，表示应用是属于哪个租户的，即由哪个租户产生的
+        """
+        # PaaS 平台上部署运行的应用，会自动内置 BKPAAS_APP_TENANT_ID 环境变量，表示应用是全租户的还是单租户的
+        tenant_id = os.environ.get("BKPAAS_APP_TENANT_ID")
+        if tenant_id is not None:
+            # 空字符串表示全租户应用，则返回 system，因为全租户应用只能在运营租户 (system) 下创建
+            return tenant_id or "system"
+
+        # 如果从环境变量获取不到，即非 PaaS 平台上运行，则需要从配置中获取
+        # 注意：对于单租户应用，BK_APP_TENANT_ID 可以不设置
+        #  对于全租户应用，BK_APP_TENANT_ID 必须设置，建议设置为 system
+        return getattr(settings, "BK_APP_TENANT_ID", "")
 
     def migrate(self):
         iam_host = getattr(settings, "BK_IAM_APIGATEWAY_URL", "")
@@ -64,6 +84,6 @@ class IAMMigrator(object):
         if not ok:
             raise exceptions.NetworkUnreachableError("bk iam ping error")
 
-        ok = do_migrate.do_migrate(data, iam_host, self._bk_app_code, self._bk_app_secret)
+        ok = do_migrate.do_migrate(data, iam_host, self._bk_app_code, self._bk_app_secret, self._bk_app_tenant_id)
         if not ok:
             raise exceptions.MigrationFailError("iam migrate fail")
